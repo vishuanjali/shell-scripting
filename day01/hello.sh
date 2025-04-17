b4732067a37f9fbca58e40f0ee2b1a61c08c39e2
root <root@DESKTOP-21ATSCV.>
2025-04-17 15:53:40 +0000

    second commit

---
+++ day01/hello.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "hello dosto"
+
+echo "As a tech enthusiast with a strong foundation in web development and DevOps, I have hands-on experience with technologies like HTML, CSS, JavaScript, React, Express, and Next.js.
+
+On the DevOps side, I actively work with tools like Linux, Git, GitHub, Docker, Kubernetes, Terraform, Jenkins, Ansible, and cloud platforms like AWS. I also have some experience with monitoring tools such as Datadog."
